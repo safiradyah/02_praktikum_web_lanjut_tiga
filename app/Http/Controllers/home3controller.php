6db673c36aaa3b7fc39eb758a7724eca2104a5b3
= <=>
2022-02-28 20:28:25 +0700

    praktikum 3

---
+++ app/Http/Controllers/home3controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class home3controller extends Controller
+{
+    public function index(){
+        return "Halaman Awal Website";
+    }
+}
